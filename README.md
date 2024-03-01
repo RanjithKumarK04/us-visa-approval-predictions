00f78b46cc835a79f6c26b1a55d3767ebc79cbdb
RanjithKumarK04 <kranjith.k04@gmail.com>
2024-03-01 22:28:21 +0530

    add requirements and setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # us-visa-approval-predictions
+
+## Git Commands
+
+``` bash
+
+git add .
+
+git commnit -m "updated"
+
+git push origin main
+
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us-visa",
+    version = "0.0.0",
+    author = "Ranjith",
+    author_email = "kranjith.k04@gmail.com",
+    packages = find_packages()
+)
